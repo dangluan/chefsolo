271f922bd1e831be68930efa35ea51c766962531
Rubify <peter@rubify.com>
2013-05-27 11:42:15 +0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chefsolo::Application.config.secret_token = '215f6ae460126bbe5b1b512f3382a7312bae166f2d28002304058049a19463e43683ba24065f4bbe8bc42b37184019bf90fe5ae59d69fa22228f389add96fcf7'
